3dfad29fad1e40ecc02dc8602d882cf65f7bb80f
Mirko-r <04mirko@libero.it>
2023-01-30 12:46:29 +0100

    Updated css of register.php, modified login.php

--- register.php
+++ register.php
@@ -1,25 +1,82 @@
 <html>
 	<head>
-		<title>
-			Registrati - Robot E-corp
-		</title>
-		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<title>Registrati - Robot E-corp</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/png" href="assets/images/icons/favicon.ico"/>
+		<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/util.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 	</head>
 	<body>
-		<form class='form' id='reg-form' action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-			<label class='text-label' for='name'>Nome e Cognome</label>
-			<input class='text-field' type='text'name='name' placeholder="Inserisci il tuo nome e cognome">
-			<label class='text-label' for='email'>E-mail</label>
-			<input class='text-field' type='text'name='email' placeholder="Inserisci la tua e-mail">
-			<label class='text-label' for='pass'>Password</label>
-			<input class='text-field' type='text'name='pass' placeholder="Inserisci la tua password">
-			<label class='text-label' for='repass'>Ripeti Password</label>
-			<input class='text-field' type='text'name='repass' placeholder="Inserisci nuovamente la tua password">
-			<label for='login'>Hai già un account?</label>
-			<input class='button-form' type='submit' name='login' value='Accedi'>
-			<input class='button-form' type='submit' name='register' value='Registrati'>
-		</form>
+		<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="assets/images/img-01.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" id='reg-form' action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+					<span class="login100-form-title">
+						Member Creation
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "First and last name is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="First and last name">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Email">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Register
+						</button>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 	</body>
+	<script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/vendor/select2/select2.min.js"></script>
+	<script src="assets/vendor/tilt/tilt.jquery.min.js"></script>
+	<script>
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+	<script src="assets/js/main.js"></script>
 <?php
 	if(isset($_POST("register"))) {
 		//TODO register
