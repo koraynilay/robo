d79251ab4d7e5e6f8dc3be92fec434af01f5e099
Mirko-r <04mirko@libero.it>
2023-02-13 13:37:33 +0100

    Added animated bg on login/signup

--- login.php
+++ login.php
@@ -16,9 +16,9 @@
 </head>
 
 <body>
-<div class="gradient"></div>
 	<div class="limiter">
 		<div class="container-login100">
+		<div class="gradient"></div>
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
 					<img src="assets/images/img-01.png" alt="IMG">
