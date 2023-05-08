2d0ef960a5d13295370c5057d950dabedde454cc
Mirko-r <04mirko@libero.it>
2023-05-08 11:27:15 +0200

    Updated card css

--- components/card.php
+++ components/card.php
@@ -1,9 +1,9 @@
 <?php
 	while($r = $q->fetch_assoc()) {
-		echo '<div class="col-12 col-md-4">';
-			echo ' <div class="card bg-dark text-light mt-8 border-0 shadow-lg rounded">';
+		echo '<div class="col mt-5">';
+			echo '<div class="card text-white bg-dark mb-3 border-light border-opacity-25 rounded-5" style="width: 18rem;">';
 				echo '<div class="card-body p-5 pt-8 pt-xl-14 pt-xxl-20 pe-xxl-10">';
-					echo '<h3 class="h2 mb-4">'.$r["name"].'</h3>';
+					echo '<h5 class="card-title mb-3">'.$r["name"].'</h5>';
 					echo '<h6 class="card-subtitle mb-2 text-muted">'.$r["price"].'€</h6>';
 					echo '<p class="card-text mb-4">'.$r["cat_name"].'</p>';
 					echo '<a class="btn btn-primary" href="#" onclick="alert(\'Funzione WIP\')">Add to cart</a>';
