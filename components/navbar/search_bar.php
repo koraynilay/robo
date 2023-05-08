a5be94a4a55ca948a72f7eaa71339c4033e60a20
Mirko-r <04mirko@libero.it>
2023-05-08 11:43:01 +0200

    Fixed colors

--- components/navbar/search_bar.php
+++ components/navbar/search_bar.php
@@ -1,6 +1,6 @@
 <?php
 	echo '<form class="form-inline my-2 my-lg-0" action="search.php" method="POST">
-                <input class="form-control mr-sm-2 rounded mt-1 bg-dark text-light" type="search" name=\'search\' placeholder="Search any product" aria-label="Search any product">
+                <input class="form-control searchbar mr-sm-2 rounded mt-1 bg-dark text-light" type="search" name=\'search\' placeholder="Search any product" aria-label="Search any product">
                 <button class="btn my-2 my-sm-0" type="submit" style="margin-right:1rem;"><i class="fa fa-search"></i></button>
               </form>';
 ?>
