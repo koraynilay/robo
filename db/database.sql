c89f981322442644140a7683b33d033ad1e72b12
koraynilay <koray.fra@gmail.com>
2023-05-08 13:20:46 +0200

    uncomment create tables in database.sql

--- db/database.sql
+++ db/database.sql
@@ -1,30 +1,30 @@
--- CREATE TABLE users (
--- 	id INT,
--- 	name VARCHAR(255),
--- 	email VARCHAR(255),
--- 	address VARCHAR(255),
--- 	hashpass VARCHAR(255),
--- 	
--- 	PRIMARY KEY (id)
--- );
--- 
--- CREATE TABLE category(
---     id INT, 
---     name VARCHAR(255),
--- 
---     PRIMARY KEY (id)
--- );
--- 
--- CREATE TABLE products(
--- 	id INT,
--- 	name VARCHAR(255),
--- 	weight INT,
--- 	price FLOAT,
--- 	IdCategory INT,
--- 	
--- 	PRIMARY KEY (id),
--- 	FOREIGN KEY (IdCategory) REFERENCES category(id)
--- );
+CREATE TABLE users (
+	id INT,
+	name VARCHAR(255),
+	email VARCHAR(255),
+	address VARCHAR(255),
+	hashpass VARCHAR(255),
+	
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE category(
+    id INT, 
+    name VARCHAR(255),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE products(
+	id INT,
+	name VARCHAR(255),
+	weight INT,
+	price FLOAT,
+	IdCategory INT,
+	
+	PRIMARY KEY (id),
+	FOREIGN KEY (IdCategory) REFERENCES category(id)
+);
 
 -- INSERT INTO `users` (`id`, `name`, `email`, `address`) VALUES
 
