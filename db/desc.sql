3d67876be655419d19eb08492f09ba59f9e89594
Zamasu992 <luppi.marco99@gmail.com>
2023-05-12 11:41:56 +0200

    add desc.sql

---
+++ db/desc.sql
@@ -0,0 +1,67 @@
+insert INTO products (descr) VALUES 
+
+("I cuscinetti a tazza importati vengono utilizzati sui giunti dello sterzo, il che non solo rende lo sterzo più flessibile, ma consente anche che lo sterzo della scatola dello sterzo si trovi nello stesso centro.
+Il suo sistema di alimentazione è costituito da 6 servomotori, che possono realizzare la dimostrazione di presa anteriore e posteriore, su e giù e sinistra e destra del robot.
+Tutti i raccordi delle staffe sono realizzati in lamiera di alluminio spessa 2 mm, che migliora efficacemente la stabilità del robot stesso.
+Lo chassis ha la forma di un disco, il che rende il manipolatore che gira a sinistra e a destra più flessibile e fluido.
+Il manipolatore 6DOF è ampiamente utilizzato nell'insegnamento universitario, nella produzione IDY"),
+("Sviluppato per gli amanti dei robot, il kit di bracci robotici 5-DOF è compatibile con Arduino IDE. Vengono forniti manuali dettagliati (PDF) e una serie di interessanti routine di codice Arduino.
+Metodi di controllo multipli - Controllo manuale (controllato ruotando la manopola del potenziometro sulla scheda del driver); Telecomando (controllato da software per PC basato su elaborazione grafica).
+Funzioni multiple: autoapprendimento, disegno, imitazione, ecc.
+Supporto tecnico forte - Forum ufficiale, blog, video e supporto tecnico a risposta rapida con e-mail.
+La scheda driver e Arduino UNO R3 sono integrati in un PCB per prestazioni più stabili."),
+("Un kit di movimento a 4 assi personalizzato per gli appassionati di robot che vogliono imparare a controllare il braccio meccanico
+2 modalità di controllo: manuale (con manopola) o tramite computer (LabVIEW).
+In grado di memorizzare circa 100 movimenti compiuti.
+I quattro servocomandi compongono e controllano i quattro giunti e le pinze hanno un doppio strato con estremità appuntite e bordi seghettati per afferrare gli oggetti in modo saldo.
+Manuale utente esaustivo e video che ti aiuteranno a costruire facilmente il tuo braccio robotico."),
+
+("Celle Samsung agli ioni di litio con potente batteria da 28 V, 2,9 Ah, larghezza di taglio 180 mm, altezza di taglio 20 mm - 60 mm (5 fasi)
+Funzione di rifinitura dei bordi per mantenere il giardino pulito e ordinato
+motore brushless per fornire prestazioni di taglio silenziose e potenti.
+Per un giardino più sano Con il pratico sistema di pacciamatura, i fili d'erba vengono falciati nel pacciame e sparsi sul prato in modo che possano essere lasciati come fertilizzante.
+Contenuto della confezione: 1 rasaerba robotizzato, 1 stazione di ricarica, cavo perimetrale da 160 m, 200 pezzi. Chiodi di montaggio/cavo di alimentazione da 9 m/9 pezzi Lame di ricambio/3 viti di ricambio/3 connettori per cavi/righello distanziatore/manuale utente
+Nota- Non trasportare il rasaerba robotizzato con il coperchio superiore. Verifica che il cavo perimetrale e la stazione di ricarica siano installati correttamente. Perché non è possibile modificare la data e l'ora di lavoro con la versione ECO. Se hai bisogno di più funzionalità, utilizza i modelli Bluetooth o Wi-Fi"),
+("Max; pendenza: 35% (20 °) tempo di ricarica: 180 min circa, livello di pressione sonora: 65 db, permette di trattare fino al 30% di superficie in più rispetto a un altro robot nello stesso tempo, tecnologia di taglio aia brevettata che consente al robot di accedere a zone ristrette e accesso difficile, sistema plug-and-play integrato; il robot inizia a tagliare premendo un pulsante e senza bisogno di programmazione
+Taglio laterale fino al bordo diametro taglio 18 cm con sistema mulching, 3 lame mobili che durano più a lungo grazie alla doppia affilatura su entrambi i lati, altezza taglio regolabile in 4 posizioni da 30 a 60 mm; programmazione multi-zona: gestisce m il taglio dell'erba fino a 4 diverse aree, calcolo del percorso di taglio ottimale e gestione di percorsi complessi, sicuro: sensori di sicurezza bloccano immediatamente l rotazione delle lame in caso di sollevamento o il ribaltamento"),
+("Un prodotto affidabile
+Un prodotto per il giardinaggio
+Un prodotto robusto e resistente"),
+
+("X-TRA PRATICO: Con DEEBOT X1 OMNI, non c'è bisogno di svuotare manualmente il contenitore della polvere. La stazione ha un sacchetto di polvere sigillato monouso con una capacità di 3,2 L che assomiglia a un cassetto. Durante la carica e dopo la pulizia, la polvere viene automaticamente estratta dal robot in questo sacchetto, dal quale non possono uscire né polvere né allergeni
+X-TRA POTENTE: DEEBOT X1 OMNI ha una potenza di aspirazione fino a 5.000Pa e un nuovissimo sistema a 2 panni che ruota fino a 180 volte al minuto con una pressione di 6N. Pulisce, strofina e aspira fino a quando le macchie più ostinate o i peli di animali domestici sono spariti - indipendentemente dalle condizioni del pavimento"),
+("Ultra Robot Aspirapolvere Lavapavimenti con Stazione Autopulente (Svuota Polvere, Pulisce Asciuga Moci) Rilevamento ostacoli 3D, 5300Pa Tappeti Peli Animali, Batteria 210m"),
+("【Esperti di robotica】 Da oltre 23 anni Ecovacs dedica la sua esperienza e innovazione alla ricerca sui robot. Oltre 20 milioni di famiglie in tutto il mondo possiedono il robot aspirapolvere Ecovacs. La missione di Ecovacs: un robot per ogni famiglia.
+【Compatibile con la stazione】 Ecovacs N8+ può conservare polvere e capelli fino a 4 settimane, riducendo notevolmente la frequenza di pulizia del contenitore della polvere. Potrai goderti la pulizia dei tuoi interni per settimane in pace."),
+
+("Robot Speaker Dancer per una festa indimenticabile: goditi la migliore musica con un tocco di divertimento e tecnologia avanzata che ti sorprenderà con i suoi movimenti"),
+("Requisiti: un dispositivo compatibile IOS o Android e l'app gratuita, Cozmo.
+Interruttore Essi nuovo Giochi e aggiornamenti frei, Più Essi suonare
+Lo sfida ad un gioco, o lo ha messo sulla modalità di individuazione per conoscere il mondo dalla sua prospettiva.
+Un robot intelligente e affascinante che Esplora il suo ambiente, ricorda e reagisce al suo ambiente, così come su di te.
+Modo completo testato per longevità e sicurezza"),
+("Emiglio reagisce muovendo anche testa e orecchie
+Si muove in 8 direzioni grazie al radiocomando
+Dotato di sensore di prossimità con il quale Emiglio riconosce la tua presenza ed entra in contatto con te
+Le braccia sono snodabili
+Emiglio comunica le sue emozioni grazie al voice changer e ai suoi occhioni
+"),
+
+("Creazioni spensierate - Con un peso inferiore a 249 g, Mini 3 Pro non richiede la registrazione nella maggior parte dei Paesi. Il design pieghevole e compatto lo rende facile da portare con te per qualsiasi avventura.
+Scatti davvero professionali - Cattura la bellezza del mondo con dettagli unici grazie ai video in 4K/60fps e alle foto da 48 MP. La fotocamera supporta doppia ISO nativa e apertura f/1.7 per immagini di qualità."),
+("【Motore senza spazzole】 Stone Holy HS175D DRON con telecamera 4K Adottare un motore senza spazzole ad alta velocità e ad alta frequenza, ha molti vantaggi, come alta velocità, basso valore calorifico, lunga vita utile e manutenzione. In modalità GPS, la mosca bassa fornisce una velocità di volo fino a 5 m/s e la marcia alta offre una velocità di volo fino a 8 m/s
+【Batteria da 7,6 V e 1700 mAh, 46 minuti】 La tensione di 7,6 V significa un forte tempo di volo e, con la batteria di riserva inclusa, il tempo di volo raggiungerà 46 minuti"),
+("【Doppio GPS e Glonass, Indietro con Una Chiave】drone con fotocamera è dotato di doppio posizionamento e sistema di navigazione GPS e Glonass, che può conoscere la sua posizione in qualsiasi momento e cogliere con precisione la destinazione del volo. Inoltre, questo drone telecomandato ha anche una funzione RTH (ritorno a casa). Quando la batteria è scarica o il segnale è perso, basta premere il pulsante e il drone tornerà automaticamente al punto di partenza originale.
+【Modalità di Volo Intelligenti】 Questo drone professionale ha modalità più intelligenti, come seguimi, volo circolare diy, volo waypoint, volo preciso in quota, ritorno automatico, controllo APP e modalità sport, ecc. La velocità massima è di 36 km/he la velocità massima di salita è di 10 m/s. I droni di alta qualità sono resistenti al vento fino al livello 4: scatta foto/video con resistenza al vento fino al livello 3, permettendoti di goderti appieno il divertimento negli spazi aperti.
+"),
+
+("Una nuova telecamera robot: Scout ti aiuta a monitorare la tua casa da lontano senza punti ciechi. Il robot video segue un percorso pre-programmato per pattugliare la tua casa 24 ore su 24, 7 giorni su 7 e trova la strada per tornare al dock di ricarica.
+Divertimento e intrattenimento: questa telecamera robot intelligente funziona come telecamera wireless per i tuoi amici a quattro zampe. Controlla la trazione integrale omnidirezionale dal tuo smartphone e guarda la tua casa in un modo completamente diverso. Il bot può anche localizzare e seguire esseri umani e animali domestici."),
+("Lente: M12 F3. 7mm F.2.0 2OW.
+Angolo di visualizzazione: H (orizzontale) 89 gradi; V verticale 52 gradi: D diagonalmente 108 gradi
+Illuminazione minima: 0,01 lux
+Ingresso / uscita: si può realizzare il citofono vocale a due vie (microfono e altoparlante integrati)
+Metodo di compressione audio: G711A"),
+("La tua telecamera IP per la casa in movimento: crociera automatica e rilevamento del movimento. Ebo SE è la scelta migliore per programmare pattuglie domestiche di routine o sorveglianza live streaming per la tua famiglia. Il sistema Ebo rileverà automaticamente le attività sospette nel campo visivo di Ebo e ti avviserà tramite l'APP Ebo.
+Visione notturna e videocamera HD 1080p con audio bidirezionale: Ebo SE fornisce immagini luminose anche durante la notte più buia. La fotocamera Full 1080p HD con potenti altoparlanti e microfono, ti consente di avere un'interazione in tempo reale con la tua famiglia.");
+;
